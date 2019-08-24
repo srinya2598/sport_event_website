5614de6ac7b0e628ac63b3f533d95f7a98501b48
srinya2598 <srinya2598@gmail.com>
2019-08-24 15:30:55 +0530

    temp

--- src/app/dashboard/components/register/register.component.spec.ts
+++ src/app/dashboard/components/register/register.component.spec.ts
@@ -1,21 +1,28 @@
 import {RegisterComponent} from './register.component';
 import {ComponentFixture, TestBed} from '@angular/core/testing';
 import {CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
+import {RegisterationController} from '../../../controller/registeration.controller';
 
 describe('Component: RegisterComponent', () => {
-    let component: RegisterComponent;
-    let fixture: ComponentFixture<RegisterComponent>;
-    beforeEach(() => {
-      TestBed.configureTestingModule({
-        imports: [],
-        declarations: [RegisterComponent],
-        schemas: [CUSTOM_ELEMENTS_SCHEMA]
-      }).compileComponents();
-      fixture = TestBed.createComponent(RegisterComponent);
-      component = fixture.componentInstance;
+  let component: RegisterComponent;
+  let fixture: ComponentFixture<RegisterComponent>;
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [],
+      declarations: [RegisterComponent],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+      providers: [
+        {
+          provide: RegisterationController,
+          useValue: {}
+        }
+      ]
+    }).compileComponents();
+    fixture = TestBed.createComponent(RegisterComponent);
+    component = fixture.componentInstance;
 
-    });
-    it('should have a defined component', () => {
-      expect(component).toBeDefined();
-    });
   });
+  it('should have a defined component', () => {
+    expect(component).toBeDefined();
+  });
+});
